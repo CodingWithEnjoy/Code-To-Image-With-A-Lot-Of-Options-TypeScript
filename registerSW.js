fdcf68ae2daf66772516dc036a63c3577be65526
Coding With Enjoy <codingwithenjoy@gmail.com>
2023-01-20 14:31:43 +0330

    commiting

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Code-To-Image-With-A-Lot-Of-Options-TypeScriptsw.js', { scope: '/Code-To-Image-With-A-Lot-Of-Options-TypeScript' })})}
